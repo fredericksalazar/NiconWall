ca643d25d64a43fc52ed6f603d2543e4647dbe28
NiconDevTeam <fredefass01@gmail.com>
2015-02-23 22:38:22 -0500

    se incorporan los Online Wallpapers mode se mejora la app para reaccionar al touch se mejora la app integrando la nueva UI para Online Wallpapers. se crea nuevo codigo Javascript para descarga, control y ajuste de Online Wallpapers

--- js/core.js
+++ js/core.js
@@ -3,26 +3,48 @@
     var name;
     var prev;
 
+    //Vamos a controlar todos los posibles eventos de toque en la pantalla a traves de los cuales
+    //el usuario podrá interactuar con los controles y recursos de la app,
+    function addTouchEvent(){        
+        window.addEventListener('load', function(){ // on page load 
+            var t=document.body.addEventListener('touchstart', function(e){
+                var obj=e.changedTouches[0];
+                if(obj.target.className==='touch'){
+                    isSelected(obj.target.id);
+                } 
+                if(obj.target.id==="more"){
+                    setOnline();
+                }
+                if(obj.target.id==='set'){
+                    setWallpaper();
+                }
+                if(obj.target.id==='back'){
+                    setLocal();
+                }
+            }, false); 
+        }, false);        
+    }
+                                
+
     //permite obtener la imàgen que el usuario a seleccionado, esta serà almacenada y pasada para ajustar como
     //Wallpaper.
-
-    function isSelected(selected){  
+    function isSelected(selected){
         name=selected;
         imagen=document.getElementById(name);
         setBorder();
     }
 
 
-     //Este metodo permite recibir la imagen seleccinada y enviarla para el ajuste como wallpaper en el sistema
-
-    function setWallpaper() { 
+    //Este metodo permite recibir la imagen seleccinada y enviarla para el ajuste como wallpaper en el sistema
+    function setWallpaper() {        
         if(imagen.naturalWidth > 0){
             var blobCanvas = document.createElement("canvas");
             var blobCanvasContext = blobCanvas.getContext("2d");        
             blobCanvas.width = screen.width;
             blobCanvas.height =screen.height;
-            blobCanvasContext.drawImage(imagen, 0,0,screen.width, screen.height);        
-            blobCanvas.toBlob(function (blob) {
+            blobCanvasContext.drawImage(imagen,0,0,screen.width,screen.height);
+            blobCanvas.toBlob(function(blob) {
+                        alert("si entro");
                         new MozActivity({
                             name: "share",
                             data: {
@@ -31,7 +53,7 @@
                                 blobs: [blob]
                             }
                         });
-                    });            
+                    });
         }
     } 
 
@@ -44,4 +66,45 @@
         img.style.border="1.8px solid white";
         prev=name;
         
+    }
+    
+
+    //Este metodo tiene como finalidad hacer la carga de todos los wallpapers almacenados en el servidor
+    //y que serán ofrecidos a los usuarios de forma Online.
+
+   function getOnlineResource(){       
+       //Ajustamos todas las variables y parámetros de coneccion al servidor
+        var server="http://niconsystem.zz.mu/NiconWall/";
+        var nameImg="th-wall-";
+        var ext=".jpg";
+        var maxFile=2;
+        var subName=0;
+        var idI="img";
+        var divImg=document.getElementById("online");
+        var onlImg=new Image();
+        //hacemos la carga de todas las imágenes del servidor
+            for(i=0;i<maxFile;i++){                
+                subName=i+1;                                
+                onlImg.id=idI+subName;
+                onlImg.className='touch';
+                onlImg.src=server+nameImg+subName+ext;
+                var wall=document.createElement("IMG");
+                wall=onlImg;
+                divImg.appendChild(wall);     
+            }
+    } 
+
+    function setOnline(){
+        document.getElementById("container").style.display='none';
+        document.getElementById('online').style.display='block';
+        document.getElementById("more").style.display='none';
+        document.getElementById("bn-back").style.display='inline';
+        getOnlineResource();
+    }
+
+    function  setLocal(){        
+        document.getElementById("online").style.display='none';
+        document.getElementById("container").style.display='inline';
+        document.getElementById("more").style.display='block';
+        document.getElementById("bn-back").style.display='none';
     }
