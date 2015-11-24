7c116645b598917adeeff8396068e04749ed6146
Frederick Adolfo Salazar Sanchez <fredefass01@gmail.com>
2015-11-23 23:14:20 -0500

    se agrega validacion de conexion a internet al momento de acceder al modo online

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
 'use strict';
-    
+
     var sizeLocal = 34;
     var sizeOnline = 26;
     var img;
@@ -8,14 +8,23 @@ window.addEventListener('DOMContentLoaded', function() {
     var name;
     var prev;
     var mode=0;
-    
+
     setLocal();
     addTouchEvent();
     changeRandom();
-    
+
+
+    /**
+      Este metodo permite ajustar  un escuchador de eventos de tipo touch a las
+      imágenes y demas objetos que interactuan en la interfaz de usuario.
+
+      Author: Frederik Adolfo Salazar Sanchez
+      Version: 2.0
+
+    */
 
     function addTouchEvent(){
-        window.addEventListener('load', function(){ // on page load
+        window.addEventListener('load', function(){
             var t=document.body.addEventListener('touchstart', function(e){
             var obj=e.changedTouches[0];
                 if(obj.target.className==='touch'){
@@ -36,9 +45,13 @@ window.addEventListener('DOMContentLoaded', function() {
         }, false);
     }
 
-    //permite obtener la imàgen que el usuario a seleccionado, esta serà almacenada y pasada para ajustar como
-    //Wallpaper.
-    
+
+    /**
+      Este metodo permite ajustar el borde  de la imágen seleccionada por el
+      usuario al momento de tocar la imagen con un evento touch, este metodo
+      hace el llamado al metodo setBorder();
+    */
+
     function isSelected(selected){
         name=selected;
         imagen=document.getElementById(name);
@@ -46,13 +59,17 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
 
-    //Este metodo permite recibir la imagen seleccinada y enviarla para el ajuste como wallpaper en el sistema
-    
+    /**
+      Este metodo es el encargado de ajustar la imagen de fondo seleccionada por
+      el usuario e invocar a MozActivity para recibir la imagen y poder ajustarla
+      como fondo de escritorio.
+    */
+
     function setWallpaper() {
         if(mode===0){
             if(imagen.naturalWidth > 0){
                 var blobCanvas = document.createElement("canvas");
-                var blobCanvasContext = blobCanvas.getContext("2d");        
+                var blobCanvasContext = blobCanvas.getContext("2d");
                 blobCanvas.width = screen.width;
                 blobCanvas.height =screen.height;
                 blobCanvasContext.drawImage(imagen,0,0,screen.width,screen.height);
@@ -91,47 +108,60 @@ window.addEventListener('DOMContentLoaded', function() {
             };
             xhr.send();
         }
-    } 
-
-
-    //Este metodo permite ajustar un borde a la imagen seleccionada o tocada por el usuario
-    
-    function setBorder(){    
-        if(prev!=null){
-             img=document.getElementById(prev);
-             img.style.boxShadow="0px 0px 0px 0px white";
-        }            
-        img=document.getElementById(name);
-        img.style.boxShadow="1px 1px 6px 2px #e74c3c";
-        prev=name;        
     }
 
 
-    //Este metodo tiene como finalidad hacer la carga de todos los wallpapers almacenados en el servidor
-    //y que serán ofrecidos a los usuarios de forma Online.
+    /**
+      Este metodo es el encargado de ajustar los estilos y sombras a la imágen
+      seleccionada, en caso de que una nueva imagen sea seleccionada ajusta la
+      imagen previa con el borde de modo normal.
+    */
+
+    function setBorder(){
+        if(prev!=null){
+             img=document.getElementById(prev);
+             img.style.boxShadow="0px 0px 0px 0px white";
+        }
+        img=document.getElementById(name);
+        img.style.boxShadow="1px 1px 6px 2px #e74c3c";
+        prev=name;
+    }
+
+
+    /**
+      Este metodo permite obtener todas las imágenes cargadas en el servidor
+      y cargandolas en el modo de visualización online, cabe señalar que estas
+      imagenes estan almacenadas dentro del hosting.
+    */
 
     function getOnlineResource(){
-        var subName=0;        
+        var subName=0;
         var divImg=document.getElementById("container");
-        
-            for(var i=0;i<sizeOnline;i++){                
-                subName=i+1; 
+
+          if(navigator.onLine){
+            for(var i=0;i<sizeOnline;i++){
+                subName=i+1;
                 var onlImg=document.createElement("img");
                 var wall = new Wallpaper(2,subName);
                 onlImg.id=wall.creaID();
                 onlImg.className='touch';
-                onlImg.src=wall.creaSRC();                
+                onlImg.src=wall.creaSRC();
                 divImg.appendChild(onlImg);
             }
+          }else{
+            divImg.innerHTML = "<p id='info'>¡The online mode fail! <br> NO Internet Acces, please verify!</p>";
+          }
     }
-    
-    
-    //este metodo se encarga de hacer la carga de todas las imagenes almacenadas de forma local en la app
-    //y cargarlas en la vista Html.
-    
-    function getLocalResource(){               
+
+
+    /**
+      Este metodo se encarga de obtener y cargar todas  las imágenes de modo
+      local distribuidas con la versión empaquetada de la app, al cargar las
+      imágenes dentro del modo local estas deben ser mostradas en la vista
+    */
+    function getLocalResource(){
         var subname=0;
-        var divLo=document.getElementById("container");        
+        var divLo=document.getElementById("container");
             for(var i=0;i<sizeLocal;i++){
                 subname=i+1;
                 var imagen = document.createElement("img");
@@ -142,70 +172,81 @@ window.addEventListener('DOMContentLoaded', function() {
                 divLo.appendChild(imagen);
             }
     }
-    
 
-    //Permite ajustar la UI de NiconWall para obtener los wallpapers desde el servidor.
-    
+
+    /**
+      Este metodo permite ajustar la vista de la app de modo local al modo
+      online y llama al metodo getOnlineResource para obtener las imagenes
+      del servidor.
+    */
+
     function setOnline(){
-        mode=1;             
+        mode=1;
         document.getElementById("more").style.display='none';
-        document.getElementById("bn-back").style.display='inline'; 
-        getOnlineResource();        
+        document.getElementById("bn-back").style.display='inline';
+        getOnlineResource();
         addTouchEvent();
     }
 
-    
-    /*
-       Este metodo permite ajustar la interfaz de NiconWall para el modo Local
+
+    /**
+      Este metodo permite ajustar  la vista de la app de modo online a modo loca
+      y carga nuevamente todas las imágenes locales a la vista.
     */
-    function setLocal(){  
+
+    function setLocal(){
         mode=0;
         document.getElementById("more").style.display='block';
-        document.getElementById("bn-back").style.display='none';        
+        document.getElementById("bn-back").style.display='none';
         getLocalResource();
     }
-    
-    
+
+
     /**
-        Este metodo tendrá como finalidad cambiar en un determinado intervalo de tiempo 
+        Este metodo tendrá como finalidad cambiar en un determinado intervalo de tiempo
         el wallpaper de la cabecera principal, usando JQuery vamos a crear el efecto y
         cambio cada 10 segundos.
     */
+
     function changeRandom() {
         //vamos a ajustar la primer imagen del random, las imagenes aletatorias podran ser tanto
         //locales como las online
         var imag;
         var imgID;
         var wall;
-        
+        var ranNumber;
         var imag=document.createElement("img");
+        var tipoImagen;
         imag.src = "/img/thumbs/NiconSystem.png";
         $('.random').append(imag);
-        
+
         setInterval(function(){
-            var tipoImagen = Math.floor(Math.random()*2);
+          if(navigator.onLine){
+            tipoImagen = 1;
+          }else{
+            tipoImagen = Math.floor(Math.random()*2);
+          }
             if(tipoImagen === 1){
-                imgID = Math.floor(Math.random()*sizeLocal);                
+                imgID = Math.floor(Math.random()*sizeLocal);
                 wall = new Wallpaper(1,imgID);
                 imag.id=wall.creaID();
-                imag.src=wall.creaSRC(); 
+                imag.src=wall.creaSRC();
             }else{
                 imgID = Math.floor(Math.random()*sizeOnline);
                 wall = new Wallpaper(2,imgID);
                 imag.id=wall.creaID();
-                imag.src=wall.creaSRC(); 
+                imag.src=wall.creaSRC();
             }
         $('.random').append(imag).hide().fadeIn('slow');
-        },6000);        
+        },6000);
     }
-});  
+});
 
 
 
-/*
-    Creamos el objeto Wallpaper el cual será el encargado de operaciones basicas en cuanto a manejo de wallpapers
-    se refiere.
-
+/**
+  Creamos el objeto Wallpaper el cual será el encargado de operaciones basicas
+  en cuanto a manejo de wallpapers se refiere.
 */
 
 function Wallpaper(tipo, id){
@@ -215,23 +256,23 @@ function Wallpaper(tipo, id){
     this.serverSrc = "http://niconsystem.zz.mu/NiconWall/";
     this.nombre = "th-wall-";
     this.ext = ".jpg";
-  
-    
+
+
     /**
        Este metodo permite crear el SRC de la imagen segun el tipo de imagen
        recibido
-    */    
+    */
     this.creaSRC = function(){
         //Si el tipo es 1 la imagen será cargada desde la url local.
-        
+
         if(this.tipo === 1){
               return this.localSrc+this.nombre+this.ident+this.ext;
            }else{
              return this.serverSrc+this.nombre+this.ident+this.ext;
-         }       
+         }
     };
-    
-    
+
+
     /**
        Este metodo permite crear el ID de cada imagen
     */
